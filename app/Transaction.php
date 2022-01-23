16b4c0eacec5fd21d7ac3e6e77156fb8a05cbd39
Eduardo <eduardo.2599@hotmail.com>
2022-01-23 15:20:01 -0300

    Adicionado no retorno de tansações a data de criação

--- app/Transaction.php
+++ app/Transaction.php
@@ -8,14 +8,14 @@ class Transaction extends Model
 {
     protected $table = 'transactions';
 
-    protected $hidden = ['account_id', 'created_at', 'updated_at'];
+    protected $hidden = ['account_id', 'updated_at'];
 
     protected $fillable = [
         'amount', 'type', 'account_id'
     ];
+
     public function account()
     {
         return $this->belongsTo(Account::class);
-
     }
 }
